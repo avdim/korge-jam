852f48de734c30e5b4896202de54fb1bb5268382
avdim <avdim@mail.ru>
2020-06-12 05:22:10 +0300

    changes in kt

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -15,8 +15,9 @@ suspend fun main() = Korge(width = 512, height = 512, bgcolor = Colors["#2b2b2b"
 	val image = image(resourcesVfs["korge.png"].readBitmap()) {
 		rotation = maxDegrees
 		anchor(.5, .5)
-		scale(.8)
+		scale(.3)
 		position(256, 256)
+		alpha = 0.5
 	}
 
 	while (true) {
