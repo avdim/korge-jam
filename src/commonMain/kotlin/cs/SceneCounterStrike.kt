a5a02f341418fd32444c40a2d9c2e9119610df65
avdim <avdim@mail.ru>
2020-06-13 20:44:45 +0300

    sniper

--- src/commonMain/kotlin/cs/SceneCounterStrike.kt
+++ src/commonMain/kotlin/cs/SceneCounterStrike.kt
@@ -2,23 +2,21 @@ package cs
 
 import MyDependency
 import SoundManager
+import com.soywiz.klock.seconds
 import com.soywiz.korge.animate.AnLibrary
 import com.soywiz.korge.animate.AnMovieClip
 import com.soywiz.korge.animate.serialization.readAni
 import com.soywiz.korge.scene.Scene
+import com.soywiz.korge.time.delay
 import com.soywiz.korge.view.*
-import com.soywiz.korge.view.filter.ColorMatrixFilter
-import com.soywiz.korge.view.filter.ShaderFilter
-import com.soywiz.korim.color.ColorTransform
 import com.soywiz.korim.color.Colors
-import com.soywiz.korim.color.RGBA
 import com.soywiz.korio.file.std.resourcesVfs
 import com.soywiz.korma.geom.Point
-import com.soywiz.korma.geom.vector.circle
 import com.soywiz.korma.geom.vector.rect
 import com.soywiz.korma.random.get
-import hexColor
 import myOnInteract
+import windows.WINDOWS_HEIGHT_D
+import windows.WINDOWS_WIDTH_D
 import kotlin.random.Random
 
 class SceneCounterStrike(val myDependency: MyDependency) : Scene() {
@@ -32,6 +30,63 @@ class SceneCounterStrike(val myDependency: MyDependency) : Scene() {
         mainTimeLine.scale = 0.7
         mainTimeLine.xy(0, 0)
 
+        var sniper: View? = null
+        fun hideSniper() {
+            if (sniper != null) {
+                removeChild(sniper)
+            }
+        }
+
+        fun showSniper(targetX: Double, targetY: Double) {
+            hideSniper()
+            sniper = container {
+                val SNIPER_SIZE = 200.0
+                val sniper = mainLibrary.createMovieClip("sniper")
+                sniper.width = SNIPER_SIZE
+                sniper.height = SNIPER_SIZE
+                sniper.x = targetX - SNIPER_SIZE / 2
+                sniper.y = targetY - SNIPER_SIZE / 2
+//            sniper.xy(targetX - SNIPER_SIZE/2, sniper.y - targetY)
+                addChild(sniper)
+
+                graphics {
+                    beginFill(Colors.BLACK, 1.0)
+                    fun safeRect(x: Double, y: Double, w: Double, h: Double) {
+                        if (w > 0 && h > 0) {
+                            rect(x, y, w, h)
+                        }
+                    }
+                    safeRect(0.0, 0.0, sniper.x, sniper.y)
+                    safeRect(sniper.x, 0.0, sniper.width, sniper.y)
+                    safeRect(sniper.x + sniper.width, 0.0, WINDOWS_WIDTH_D - sniper.x - sniper.width, sniper.y)
+
+                    safeRect(0.0, sniper.y, sniper.x, sniper.height)
+                    safeRect(
+                        sniper.x + sniper.width,
+                        sniper.y,
+                        WINDOWS_WIDTH_D - sniper.x - sniper.width,
+                        sniper.height
+                    )
+
+                    safeRect(0.0, sniper.y + sniper.height, sniper.x, WINDOWS_HEIGHT_D - sniper.y + sniper.height)
+                    safeRect(
+                        sniper.x,
+                        sniper.y + sniper.height,
+                        sniper.width,
+                        WINDOWS_HEIGHT_D - sniper.y + sniper.height
+                    )
+                    safeRect(
+                        sniper.x + sniper.width,
+                        sniper.y + sniper.height,
+                        WINDOWS_WIDTH_D - sniper.x - sniper.width,
+                        WINDOWS_HEIGHT_D - sniper.y + sniper.height
+                    )
+                    endFill()
+                }
+            }
+
+        }
+
         val terrorist = mainTimeLine["terrorist"] as AnMovieClip
         terrorist.timelineRunner.gotoAndPlay("default")
         val terroristMask = terrorist["my_mask"]
@@ -41,21 +96,12 @@ class SceneCounterStrike(val myDependency: MyDependency) : Scene() {
                 terrorist.timelineRunner.gotoAndPlay("fire")
 //                terrorist.timelineRunner.gotoAndPlay("die")
                 SoundManager.csAwp.play()
+                showSniper(terrorist.globalX, terrorist.globalY)
+                delay(2.seconds)
+                hideSniper()
             }
         }
 
-        container {
-            graphics {
-                beginFill(Colors.WHITE, 1.0)
-                rect(0.0, 0.0, 1000.0, 1000.0)
-                endFill()
-                beginFill(Colors.BLACK, 1.0)
-                circle(300.0, 300.0, 100.0)
-                endFill()
-            }.blendMode = BlendMode.ALPHA
-//            colorTransform = ColorTransform.Multiply(0.5, 0.5, 0.5, 1.0)
-//            solidRect(1000.0, 1000.0, Colors.BLACK).blendMode = BlendMode.MULTIPLY
-        }
 
         if (false) {
             val row1 = mainTimeLine.get("row1")
